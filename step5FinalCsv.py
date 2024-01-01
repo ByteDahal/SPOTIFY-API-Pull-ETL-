45bde0a1366760da6c4fb72d9be1f1b884e5c87a
Amrit Dahal <105136932+ByteDahal@users.noreply.github.com>
2024-01-01 11:17:26 +0545

    Add files via upload

--- step5FinalCsv.py
+++ step5FinalCsv.py
@@ -1,14 +1,14 @@
 from step4Pyspark import *
-result = df_artist \
+result_df = df_artist \
     .join(df_album, col("artist_name_artist") == col("artist_name_albums"), "left") \
     .join(df_playlist, col("artist_name_artist") == col("playlist_name"), "left") \
     .join(df_track, col("artist_name_artist") == col("artist_names"), "left")
 
-result.show()
+result_df.show()
 
-result.write.csv('z7Combined_data.csv', header=True, mode='overwrite')
+result_df.write.csv('z7Combined_data.csv', header=True, mode='overwrite')
 
-spark.stop()
+
 #----------------------------Explain-------------------------
 # df_artist \
 #     .join(df_album, col("artist_name_artist") == col("artist_name_albums"), "left") \
@@ -17,3 +17,9 @@ spark.stop()
 #---suruma artist ra album ko left join based on matching column
 #---ani tes bata aayeko result ra playlist ko left join  based on matching column
 #---ani tesbata aayeko result ra track ko left join track ko based on matching column
+# Assuming 'result_df' is your DataFrame
+result_df = result_df.drop("playlist_name","owner_name","total_track","track_name","artist_names","album_name","available_markets","total_tracks","duration_ms","populartity_track","release_year_tracks")
+#mathiko columns haruma null value thiye so hataye
+result_df = result_df.dropna()
+#aba row hataucha null bhayeka
+result_df.write.csv('z7Combined_data2.csv', header=True, mode='overwrite')
