6b5417ac4b871371c646cd69d2e2f47ad2d92663
Amrit Dahal <105136932+ByteDahal@users.noreply.github.com>
2024-01-09 11:30:04 +0545

    Add files via upload

--- testings.py
+++ testings.py
@@ -30,11 +30,14 @@ spark = SparkSession.builder \
     .config("spark.jars", "C:\\mysql-connector-j-8.2.0.jar") \
     .getOrCreate()
 
-#csv file path
+# CSV file read and create a temporary raw table
 csv_file_path = "z1SearchByArtist.csv"
+raw_df = spark.read.csv(csv_file_path, header=True, inferSchema=True)
+raw_df = raw_df.drop("href").dropna()
+raw_df.createOrReplaceTempView("stage_table")
 
 #------------------------------------------DATA IMPORT STAGE------------------------------------------
-#Create tables in database if it doesnot already exist
+# Create tables in the database if they do not already exist
 def create_table():
     create_table_queries = [
         """
@@ -51,15 +54,24 @@ def create_table():
         """,
         """
         CREATE TABLE IF NOT EXISTS fact_table (
-            artist_id INT,
+            artist_id varchar(255),
             popularity VARCHAR(255),
             genre_id INT
         );
-        """]
+        """,
+        """
+        CREATE TABLE IF NOT EXISTS dim_artist_name_generator(
+            artist_id VARCHAR(255)
+        );
+        """
+    ]
     # Execute each CREATE TABLE statement in MySQL
     for query in create_table_queries:
         cursor.execute(query)
-    connection.commit()
+
+# Execute the function to create tables in MySQL
+create_table()
+
 
 # Execute the function to create tables in MySQL
 create_table()
@@ -75,58 +87,83 @@ def import_table(table_name):
     .option("password", mysql_config["password"]) \
     .load() \
     .createOrReplaceTempView(table_name)
+    
+import_table("dim_artist_name_generator")
 import_table("dim_artist_name")
 import_table("dim_genres")
 import_table("fact_table")
 
-# CSV file read and create a temporary raw table
-csv_file_path = "z1SearchByArtist.csv"
-raw_df = spark.read.csv(csv_file_path, header=True, inferSchema=True)
-raw_df = raw_df.drop("href").dropna()
-raw_df.createOrReplaceTempView("stage_table")
 
 #------------------------------------------TABLE MANIPULATION STAGE------------------------------------------
-# Query for dim_artist_name
-query_dim_artist_name = '''
-    SELECT s.id as artist_id, s.artist_name
-    FROM stage_table s
+# Query for creating a temporary table if it doesn't exist
+temp_dim_artist_name_table_query = '''
+CREATE TEMPORARY VIEW temp_dim_artist_name_table
+AS 
+SELECT s.id AS artist_id 
+FROM stage_table s
+GROUP BY s.id
+'''
+spark.sql(temp_dim_artist_name_table_query)
+
+# query for dim artist generator
+query_dim_artist_name_generator = '''
+    SELECT tda.artist_id
+    FROM temp_dim_artist_name_table tda
     LEFT JOIN dim_artist_name dan
-    ON s.id = dan.artist_id
+    ON tda.artist_id = dan.artist_id
     WHERE dan.artist_id IS NULL
 '''
-dim_artist_name_df = spark.sql(query_dim_artist_name)
+dim_artist_name_generator_df = spark.sql(query_dim_artist_name_generator)
+dim_artist_name_generator_df.createOrReplaceTempView("temp_dim_artist_name_generator")
 
-# Query for dim_genres
-query_dim_genre = '''
-    SELECT monotonically_increasing_id() as genre_id, s.genres
-    FROM stage_table s
-    LEFT JOIN dim_genres dg
-    ON s.genres = dg.genres
-    WHERE dg.genres IS NULL
+
+delete_query = '''
+DELETE FROM dim_artist_name
+WHERE artist_id IN (SELECT artist_id FROM temp_dim_artist_name_generator)
 '''
-dim_genre_df = spark.sql(query_dim_genre)
+spark.sql(delete_query)
 
-# Insert data into fact_table from stage_table and dim_genres
-query_insert_fact = """
-    SELECT dan.artist_id, s.popularity, dg.genre_id
-    FROM stage_table s
-    JOIN dim_artist_name dan
-    ON s.id = dan.artist_id
-    JOIN dim_genres dg 
-    ON s.genres = dg.genres
-"""
-fact_df = spark.sql(query_insert_fact)
 
-#------------------------------------------TABLE INSERTION STAGE------------------------------------------
-# Write to MySQL database
-def insert_into_db(df_name,table_name):
-    df_name.write.format("jdbc").option("url", mysql_config["url"]) \
-        .option("driver", mysql_config["driver"]) \
-        .option("dbtable",table_name) \
-        .option("user", mysql_config["user"]) \
-        .option("password", mysql_config["password"]) \
-        .mode("overwrite") \
-        .save()
-insert_into_db(dim_artist_name_df,"dim_artist_name")
-insert_into_db(dim_genre_df,"dim_genres")
-insert_into_db(fact_df,"fact_table")
+# query_dim_artist_name = '''select tda.artist_id, tda.artist_name
+# from temp_dim_artist_name_table tda
+# inner join dim_artist_name dan
+# on tda.artist_id = dan.artist_id
+# where dan.artist_id is null
+# '''
+# dim_artist_name_df = spark.sql(query_dim_artist_name)
+
+# # Query for dim_genres
+# query_dim_genres = '''
+#     SELECT monotonically_increasing_id() as genre_id, s.genres
+#     FROM stage_table s
+#     LEFT JOIN dim_genres dg
+#     ON s.genres = dg.genres
+#     WHERE dg.genres IS NULL
+# '''
+# dim_genre_df = spark.sql(query_dim_genres)
+
+# # Insert data into fact_table from stage_table and dim_genres
+# query_insert_fact = """
+#     SELECT dan.artist_id, s.popularity, dg.genre_id
+#     FROM stage_table s
+#     JOIN dim_artist_name dan
+#     ON s.id = dan.artist_id
+#     JOIN dim_genres dg 
+#     ON s.genres = dg.genres
+# """
+# fact_df = spark.sql(query_insert_fact)
+
+# #------------------------------------------TABLE INSERTION STAGE------------------------------------------
+# # Write to MySQL database
+# def insert_into_db(df_name,table_name):
+#     df_name.write.format("jdbc").option("url", mysql_config["url"]) \
+#         .option("driver", mysql_config["driver"]) \
+#         .option("dbtable",table_name) \
+#         .option("user", mysql_config["user"]) \
+#         .option("password", mysql_config["password"]) \
+#         .mode("overwrite") \
+#         .save()
+# insert_into_db(dim_artist_name_generator_df,"dim_artist_name_generator")
+# insert_into_db(dim_artist_name_df,"dim_artist_name")
+# insert_into_db(dim_genre_df,"dim_genres")
+# insert_into_db(fact_df,"fact_table")
